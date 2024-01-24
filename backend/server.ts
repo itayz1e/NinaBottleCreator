b7d13a9899fbf7c28a335ab39c1da89ea719f3d4
itayz1e <itayz1e@gmail.com>
2024-01-24 20:14:18 +0200

    fix port env

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ import cors from "cors";
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.use(express.json());
 app.use(cors());
 
